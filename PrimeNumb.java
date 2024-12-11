f9671cd795e310657f547c7c5238cf82645fdd84
BANTU HARSHITH <22h51a0504@gmail.com>
2024-12-11 18:42:45 +0530

    hello

---
+++ PrimeNumb.java
@@ -0,0 +1,18 @@
+public class PrimeNumb {
+    public static void main(String[] args) {
+        int limit = 50; // Change this to set the range
+        for (int num = 2; num <= limit; num++) {
+            if (isPrime(num)) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+
+    static boolean isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
